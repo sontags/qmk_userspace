c009808dc88bf33751765a3036db807baaf2e5a2
Daniel Menet <daniel@sonta.gs>
2025-06-07 13:42:50 +0200

    feat: corne dvorak layout

---
+++ keyboards/crkbd/rev4_1/mini/keymaps/sontags/rules.mk
@@ -0,0 +1,6 @@
+LTO_ENABLE = yes            # Link Time Optimization enabled
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+AUDIO_ENABLE = no           # Audio output
+RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
+RGB_MATRIX_ENABLE = no      # Enable RGB matrix.
+SEND_STRING_ENABLE = yes    # Required for macros
